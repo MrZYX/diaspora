04b3ae5c2700a16912698812b4148a87b70a95c7
Jonne Haß <me@jhass.eu>
2019-09-08 10:48:40 +0200

    Sentry: Ignore ActionController::UnknownFormat

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,5 +3,6 @@ if AppConfig.environment.sentry_dsn.present?
     config.dsn = AppConfig.environment.sentry_dsn.to_s
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
     config.async = ->(event) { Workers::Sentry.perform_async(event) }
+    config.excluded_exceptions += [ActionController::UnknownFormat]
   end
 end
